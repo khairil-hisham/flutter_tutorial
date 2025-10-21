540e304b4b1f5a623b048998d5b67cad6c032052
Khairil Hisham <khairil.h0703@gmail.com>
2025-10-22 00:03:04 +0800

    digital clock on about page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
 void main() => runApp(const MyApp());
 
@@ -60,6 +61,9 @@ class _AboutPageState extends State<AboutPage>{
           child: Column(
             children:[
               Text("About page!"),
+              SizedBox(height:20),
+              DigitalClock(),
+              SizedBox(height:20),
               ElevatedButton(
                 onPressed: () {
                   print('clicked');
@@ -70,6 +74,45 @@ class _AboutPageState extends State<AboutPage>{
             ]
           )
         )
+    );
+  }
+}
+
+
+
+class DigitalClock extends StatefulWidget {
+  @override
+  _DigitalClockState createState() => _DigitalClockState();
+}
+
+class _DigitalClockState extends State<DigitalClock> {
+  late Timer _timer;
+  late DateTime _now;
+
+  @override
+  void initState() {
+    super.initState();
+    _now = DateTime.now();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _now = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "${_now.hour.toString().padLeft(2, '0')}:"
+      "${_now.minute.toString().padLeft(2, '0')}:"
+      "${_now.second.toString().padLeft(2, '0')}",
+      style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
     );
   }
 }
